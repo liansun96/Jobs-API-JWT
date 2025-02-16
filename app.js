5ec8160eb96ebfee75f5803825eb9855483982a1
liansun96 <liansun0203@gmail.com>
2025-02-16 16:10:26 +0630

    apply extra security packages

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+//Extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const connectDB = require('./db/connect')
 
 const authRouter = require('./routes/auth')
@@ -11,8 +17,20 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const authMiddleware = require('./middleware/authentication')
 
+app.use(rateLimiter(
+    {
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+        standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+        // store: ... , // Redis, Memcached, etc. See below.
+    }
+))
 
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 
 app.use('/api/v1/auth' , authRouter)
---
+++ controller/jobs.js
@@ -0,0 +1,63 @@
+const { StatusCodes } = require("http-status-codes")
+const Job = require("../models/Job")
+const  jwt  = require("jsonwebtoken")
+const NotFoundError = require("../errors/not-found")
+const BadRequestError = require("../errors/bad-request")
+
+const getAllJobs = async (req , res) => {
+    const jobs = await Job.find({createdBy : req.user.userId}).sort('-createdAt')
+    
+    res.status(StatusCodes.OK).json({nbHits : jobs.length , jobs})
+}
+
+const getJob = async (req , res) => {
+      
+    const {user : {userId} , params : {id : jobId}} = req
+
+    const job = await Job.findById({_id : jobId , createdBy : userId})
+    if(!job){
+        throw new NotFoundError(`No Job Wiht id : ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
+}
+
+const createJob= async(req, res) => {    
+    req.body.createdBy = req.user.userId
+    
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
+}
+
+const updateJob = async(req , res) => {
+    const {user : {userId} , params : {id : jobId} , body : {company , position}} = req
+
+    if(!company || !position ){
+        throw new BadRequestError('Company and Position Field cannot be empty!')
+    }
+
+    const job = await Job.findByIdAndUpdate(
+        {_id : jobId , createdBy : userId},
+        req.body,
+        {new : true}
+    )
+
+    if(!job){
+        throw new NotFoundError(`No job wiht id : ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
+}
+
+const deleteJob = async(req , res) => {
+    const {user : {useId} , params : {id : jobId}} = req
+
+    const job = await Job.findByIdAndDelete({_id : jobId})
+    if(!job) {
+        throw new NotFoundError(`No Job Wiht Id : ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
+}
+
+module.exports = {getAllJobs , getJob , createJob , updateJob , deleteJob }
