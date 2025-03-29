cb842a76449d1439833d288e694db99632979658
liansun96 <liansun0203@gmail.com>
2025-03-29 13:26:40 +0630

    update auth middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -19,23 +19,4 @@ const authMiddleware = async (req , res , next ) => {
     }
 }
 
-const auht = async(req , res , next) => {
-     const authHeader = req.header.authorization
-
-     if(!authHeader || !authHeader.startsWiht('Bearer ')){
-          throw new UnauthenticatedError('Authentication Invilid')
-     }
-
-     const token = authHeader.split(' ')[1]
-
-     try {
-          const payload = jwt.verify(token , process.env.JWT_SECRET)
-          req.user = {userId : payload.userId , name : payload.name }
-     } catch (error) {
-          throw new UnauthenticatedError('Authentication Invlilid')
-     }
-}
-
-
-
 module.exports = authMiddleware
